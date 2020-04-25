1b709b686f8c33a1bbfd1e80aeaccf8b1bafa993
Poplar Wang <wang.poplar@gmail.com>
2020-04-25 01:24:44 -0400

    Server Save on 2020-04-25

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-java -Xmx1336M -Xms1336M -jar server.jar nogui
+@echo off
+title Village Minecraft Server Console
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+git add *
+git commit -m "Server Save on %date%"
+git push
+exit
