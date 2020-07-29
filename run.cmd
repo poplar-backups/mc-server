14ad6bd4bfb6aad006912ce0c3afc2299b119bfb
Poplar Wang <snowlion18@gmail.com>
2020-07-28 22:58:43 -0400

    Update to continuous run batch file

--- run.cmd
+++ run.cmd
@@ -1,5 +1,15 @@
+:start
 java -Xmx3072M -Xms3072M -jar server.jar nogui
+
+:backup
 git add *
 git commit -m "Server Save on %date% at %time%"
 git push
-exit
+
+:reboot
+@echo off
+echo Server is currently off!
+echo Press any key to restart the server.
+pause >nul
+@echo on
+goto :top
